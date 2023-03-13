473d1a95628f7bb0dd6eb64b418b3c59a1176e8e
Trịnh Minh Nhật <trinhmnhat@gmail.com>
2023-03-13 07:00:00 +0700

    feat: implement search and filter monsters

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends Component {
 
         this.state = {
             monsters: [],
+            searchValue: '',
         };
+
+        console.log('constructor');
     }
 
     componentDidMount() {
@@ -20,16 +23,36 @@ class App extends Component {
                         return { monsters: users };
                     },
                     () => {
-                        console.log('this.state: ', this.state);
+                        // console.log('this.state: ', this.state);
                     },
                 ),
             );
+
+        console.log('componentDidMount');
     }
 
     render() {
+        const filteredMonsters = this.state.monsters.filter((monster) => {
+            console.log('filteredMonsters: ');
+            return monster.name.toLowerCase().includes(this.state.searchValue);
+        });
+
+        console.log('render');
         return (
             <div className="App">
-                {this.state.monsters.map((monster) => (
+                <input
+                    type="text"
+                    className="search-box"
+                    placeholder="Search Monsters..."
+                    onChange={(e) => {
+                        const searchValue = e.target.value.toLowerCase();
+
+                        this.setState(() => {
+                            return { searchValue };
+                        });
+                    }}
+                />
+                {filteredMonsters.map((monster) => (
                     <h1 key={monster.id}>{monster.name}</h1>
                 ))}
             </div>
